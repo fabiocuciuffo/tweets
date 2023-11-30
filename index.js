33b0af341dc5da25e256d9caf19d10fffc96aca3
fabiocuciuffo <cuciuffofab@gmail.com>
2023-11-30 13:47:43 +0100

    consigne 2

--- index.js
+++ index.js
@@ -32,28 +32,31 @@ const tweets = [
     },
 ]
 
+//objectif n°2 : afficher tous les tweets
+
+//récupération du UL présent dans le DOM de la page grâce à son ID "tweets-list"
 const tweetsContainer = document.getElementById("tweets-list")
 
-//objectif n°1 : afficher le premier tweet
+//même processus que précédemment sauf que cette fois ci ce n'est plus l'index 0 du tableau tweets que l'on vient récupérer pour assigner le contenu des éléments P mais dynamique l'élément de l'itération de la boucle forEach qui représente un objet
+tweets.forEach((element) => {
+    let tweetLi = document.createElement("li")
+    tweetLi.classList.add("tweet")
 
-//création de l'élément LI
-let tweetLi = document.createElement("li")
-//ajout de la classe "tweet" au LI
-tweetLi.classList.add("tweet")
-//création d'un élément P pour le content
-let content = document.createElement("p")
-//ajout de la classe "content" à P
-content.classList.add("content")
-//assignassion
-content.innerText = tweets[0].content
-let likes = document.createElement("p")
-likes.classList.add("likes")
-likes.innerText = tweets[0].likes
-let author = document.createElement("p")
-author.classList.add("author")
-author.innerText = tweets[0].author
+    let content = document.createElement("p")
+    content.classList.add("content")
+    content.innerText = element.content
 
-tweetLi.appendChild(content)
-tweetLi.appendChild(likes)
-tweetLi.appendChild(author)
-tweetsContainer.appendChild(tweetLi)
+    let likes = document.createElement("p")
+    likes.classList.add("likes")
+    likes.innerText = element.likes
+
+    let author = document.createElement("p")
+    author.classList.add("author")
+    author.innerText = element.author
+
+    tweetLi.appendChild(content)
+    tweetLi.appendChild(likes)
+    tweetLi.appendChild(author)
+
+    tweetsContainer.appendChild(tweetLi)
+})
